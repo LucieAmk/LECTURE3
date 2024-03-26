59c6a9fd895e3c3dbe92513247f9798cd036e4d5
Lucy Isichei <luciewere204@gmail.com>
2024-03-26 14:24:23 -0700

    my assignment

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Hello World")
+let a = 100;
+let x = 1;
+while (x <= 10) {
+    console.log(x);
+    x = x + 1
+}
+for(let i = 10; i > 0; i = i-1){
+    console.log(i);
+}
+let name = "Lucy"
+let num = 2;
+let ids = [2,4,5,10,1];
+console.log(ids)
+console.log(ids[4]);
+ids[4]=25 
+console.log(ids);
+ids[2]=20
+ids.push(20)
+console.log(ids);
+ids.unshift(14)
+console.log(ids)
+let nums=[];
+for(let i=1; i<=10; i++){
+    nums.push(i)
+}
+console.log(nums);
